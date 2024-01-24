9548c35b8f7148781e5698f4f0a36ed0d3cedb2a
darrie7 <darnell.r.kluit@gmail.com>
2024-01-24 21:39:56 +0100

    Update torrentfinished2.py

--- torrentfinished2.py
+++ torrentfinished2.py
@@ -6,6 +6,9 @@ from glob import glob
 import requests
 from babelfish import Language
 from subliminal import download_best_subtitles, region, save_subtitles, scan_video
+from time import sleep
+from asyncio import run, to_thread, sleep, gather
+from random import randint
 
 VIDEO_EXTENSIONS = [
     ".avi", ".mp4", ".mkv", ".mpg",
@@ -13,32 +16,31 @@ VIDEO_EXTENSIONS = [
     ".wmv", ".flv", ".3gp",".3g2", ".swf", ".mswmm"
 ]
 
-def download_subs(file_path):
-    region.configure('dogpile.cache.dbm', arguments={'filename': 'cachefile.dbm'})
-    subtitles = download_best_subtitles([file_path], {Language('eng')})
+async def download_subs(file_path):
+    await sleep(randint(2, 45))
+    region.configure('dogpile.cache.dbm', arguments={'filename': 'cachefile.dbm'}, replace_existing_backend=True)
+    video = scan_video(file_path)
+    subtitles = await to_thread(download_best_subtitles, [video], {Language('eng')})
     # save them to disk, next to the video
-    save_subtitles(file_path, subtitles[file_path])
+    save_subtitles(video, subtitles[video])
+    return
 
-def func(torrent):
+async def funcsion(torrent):
     if os.path.isfile(torrent):
         if any(torrent.lower().endswith(ext) for ext in VIDEO_EXTENSIONS):
-            video = scan_video(torrent)
-            download_subs(video)
+            await download_subs(torrent)
         return
-    for filename in glob(f'{torrent}/**', recursive=True):
-        if os.path.isfile(filename) and any(filename.lower().endswith(ext) for ext in VIDEO_EXTENSIONS):
-            video = scan_video(filename)
-            download_subs(video)
+    await gather(*[download_subs(file) for file in glob(f'{torrent}/**/*', recursive=True) if (os.path.isfile(file) and any(file.lower().endswith(ext) for ext in VIDEO_EXTENSIONS))])
     return
 
 if __name__ == "__main__":
     torrentid = sys.argv[1]
     torrentname = sys.argv[2]
     torrentpath = sys.argv[3]
-    func(f"{torrentpath}/{torrentname}")
+    run(funcsion(f"{torrentpath}/{torrentname}"))
+    # os.system(f"zenity --info --no-wrap --display=:0.0 --text='{torrentname} finished downloading';exit")
     data = str({ "username": "Emby", "content": f"{torrentname} finished downloading" }).replace("'", '"')
     apikey = "embyapi"
     os.system(f"/usr/bin/deluge-console 'rm -c {torrentid}; exit'")
     os.system(f"curl  --data '' http://localhost:8096/Emby/Library/Refresh?api_key={apikey}")
-    os.system(f"curl -H 'Content-Type: application/json' -d '{data}' 'https://discord.com/api/webhooks/{server}/{webhook}'")
-    # os.system(f"zenity --info --no-wrap --display=:0.0 --text='{torrentname} finished downloading';exit")
+    os.system(f"curl -H 'Content-Type: application/json' -d '{data}' 'https://discord.com/api/webhooks/server/channel'")
